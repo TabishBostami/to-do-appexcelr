5ee19cd823eea219a9b051e820a4ff81eb863255
Tabish bostami <tabish.bostami042000@gamil.com>
2024-01-12 11:16:56 +0530

    Save local changes before merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./App.css";
-
-
-
-
-
 import Navbar from "./Components/Navbar";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Info from "./pages/Info";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // SPA --- single page application
 
@@ -16,7 +12,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cont" element={<Contact />} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/info" element={<Info/>} />
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Info.js
@@ -0,0 +1,5 @@
+export default function Info() {
+  return (
+    <div>information page</div>
+  )
+}
